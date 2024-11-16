6ed9af2df739ef44e1356a0833b70732302b6ffa
jeyaprakashp4587 <jeyaprakashp022@gmail.com>
2024-11-16 17:44:24 +0530

    update

--- Api.js
+++ Api.js
@@ -1,4 +1,4 @@
-const Api = 'https://codecampusserver-r6gw.onrender.com';
+const Api = 'http://192.168.42.17:8080';
 module.exports = Api;
 // https://codecampusserver-r6gw.onrender.com
 // 192.168.10.90  redmi 10 ip
--- Screens/Placement.jsx
+++ Screens/Placement.jsx
@@ -1,19 +1,29 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {Dimensions, Image, StyleSheet, Text, View} from 'react-native';
 import HeadingText from '../utils/HeadingText';
 import {Colors, pageView} from '../constants/Colors';
 import Moment from 'moment';
 
 const Placement = () => {
-  console.log(Moment().format('YYYY-MM-DDTHH:mm:ss'));
+  const {width, height} = Dimensions.get('window');
   return (
     <View style={pageView}>
       {/* header */}
-
-      <HeadingText text="Placement" />
-
-      {/* jobs lists */}
-      {/* <Text>We will meet on another date bye dude :)</Text> */}
+      <View style={{paddingHorizontal: 15}}>
+        <HeadingText text="Placement" />
+        <Text style={{color: Colors.mildGrey, letterSpacing: 2}}>
+          We are working on it :)
+        </Text>
+        <Image
+          source={{uri: 'https://i.ibb.co/fQhgqVz/rb-2148817994.png'}}
+          style={{
+            width: width * 0.7,
+            height: height * 0.3,
+            alignSelf: 'center',
+            marginTop: height * 0.2,
+          }}
+        />
+      </View>
     </View>
   );
 };
