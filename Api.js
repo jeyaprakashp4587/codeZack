e16aa7b63a40e413e7e88fa7f675de2e3f3f6a27
jeyaprakashp4587 <jeyaprakashp022@gmail.com>
2024-12-23 09:31:01 +0530

    update

--- Api.js
+++ Api.js
@@ -1,18 +1,18 @@
 const Api = __DEV__
   ? 'http://192.168.43.90:8080'
-  : 'https://codezackfunctionserver-1.onrender.com';
+  : 'https://codezackserver.onrender.com';
 const SocketApi = __DEV__
   ? 'http://192.168.43.90:8080'
-  : 'https://codezackfunctionserver-1.onrender.com';
+  : 'https://codezackserver.onrender.com';
 const loginApi = __DEV__
   ? 'http://192.168.43.90:8080'
-  : 'https://codezackfunctionserver-1.onrender.com';
+  : 'https://codezackserver.onrender.com';
 const profileApi = __DEV__
   ? 'http://192.168.43.90:8080'
-  : 'https://codezackfunctionserver-1.onrender.com';
+  : 'https://codezackserver.onrender.com';
 const functionApi = __DEV__
   ? 'http://192.168.43.90:8080'
-  : 'https://codezackfunctionserver-1.onrender.com';
+  : 'https://codezackserver.onrender.com';
 
 // -----
 module.exports = {Api, loginApi, SocketApi, profileApi, functionApi};
