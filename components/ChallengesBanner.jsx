356d7c311f97f39b3e7d8079764a402001efd21a
jeyaprakashp4587 <jeyaprakashp022@gmail.com>
2025-04-26 20:52:19 +0530

    ui changes

--- components/ChallengesBanner.jsx
+++ components/ChallengesBanner.jsx
@@ -11,77 +11,91 @@ const ChallengesBanner = () => {
   const naviagte = useNavigation();
   const [showBanner, setShowBanner] = useState(false);
   useEffect(() => {
-    try {
-      const clickedcount = AsyncStorage.getItem('clickcount');
-      if (clickedcount == 'counted') {
-        setShowBanner(true);
+    const fetchClickedCount = async () => {
+      try {
+        const clickedcount = await AsyncStorage.getItem('clickedcount');
+        console.log(clickedcount);
+        // if (clickedcount === 'counted') {
+        //   // setShowBanner(true);
+        //   // console.log('ok', clickedcount);
+        // }
+        {
+          clickedcount === 'counted' ? null : (
+            <View
+              style={{
+                marginHorizontal: 15,
+                padding: 10,
+                borderRadius: 10,
+                flexDirection: 'row',
+                alignItems: 'center',
+                elevation: 2,
+                paddingHorizontal: 20,
+                marginVertical: 15,
+                backgroundColor: 'white',
+              }}>
+              <View
+                style={{
+                  flex: 1,
+                  borderWidth: 0,
+                  flexDirection: 'column',
+                  rowGap: 10,
+                }}>
+                <Text
+                  style={{
+                    // width: '45%',
+                    fontSize: width * 0.04,
+                    fontFamily: Font.Medium,
+                    lineHeight: 25,
+                    letterSpacing: 0.3,
+                  }}>
+                  Get More Web & App Challenges
+                </Text>
+                <TouchableOpacity
+                  onPress={async () => {
+                    naviagte.navigate('Code');
+                    await AsyncStorage.setItem('clickedcount', 'counted');
+                    console.log(showBanner);
+                  }}
+                  style={{
+                    // borderWidth: 0.8,
+                    flexDirection: 'row',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    borderRadius: 20,
+                    width: '70%',
+                    borderColor: Colors.violet,
+                    backgroundColor: Colors.violet,
+                  }}>
+                  <Text
+                    style={{
+                      fontFamily: Font.Medium,
+                      letterSpacing: 0.6,
+                      fontSize: width * 0.033,
+                      padding: 7,
+                      textAlign: 'center',
+                      color: Colors.white,
+                    }}>
+                    Explore
+                  </Text>
+                </TouchableOpacity>
+              </View>
+              <FastImage
+                source={{
+                  uri: 'https://i.ibb.co/Z6gZmMZ2/12083375-Wavy-Bus-20-Single-04.jpg',
+                  priority: FastImage.priority.high,
+                }}
+                style={{width: width * 0.35, aspectRatio: 1}}
+                resizeMode="contain"
+              />
+            </View>
+          );
+        }
+      } catch (error) {
+        console.error('Error fetching clicked count:', error);
       }
-    } catch (error) {}
+    };
+    fetchClickedCount();
   }, []);
-  if (showBanner) return null;
-  return (
-    <View
-      style={{
-        marginHorizontal: 15,
-        padding: 10,
-        borderRadius: 10,
-        flexDirection: 'row',
-        alignItems: 'center',
-        elevation: 2,
-        paddingHorizontal: 20,
-        marginVertical: 15,
-        backgroundColor: 'white',
-      }}>
-      <View
-        style={{flex: 1, borderWidth: 0, flexDirection: 'column', rowGap: 10}}>
-        <Text
-          style={{
-            // width: '45%',
-            fontSize: width * 0.04,
-            fontFamily: Font.Medium,
-            lineHeight: 25,
-            letterSpacing: 0.3,
-          }}>
-          Get More Web & App Challenges
-        </Text>
-        <TouchableOpacity
-          onPress={async () => {
-            naviagte.navigate('Code');
-            await AsyncStorage.setItem('clickedcount', 'counted');
-          }}
-          style={{
-            // borderWidth: 0.8,
-            flexDirection: 'row',
-            justifyContent: 'center',
-            alignItems: 'center',
-            borderRadius: 20,
-            width: '70%',
-            borderColor: Colors.violet,
-            backgroundColor: Colors.violet,
-          }}>
-          <Text
-            style={{
-              fontFamily: Font.Medium,
-              letterSpacing: 0.6,
-              fontSize: width * 0.033,
-              padding: 7,
-              textAlign: 'center',
-              color: Colors.white,
-            }}>
-            Explore
-          </Text>
-        </TouchableOpacity>
-      </View>
-      <FastImage
-        source={{
-          uri: 'https://i.ibb.co/Z6gZmMZ2/12083375-Wavy-Bus-20-Single-04.jpg',
-          priority: FastImage.priority.high,
-        }}
-        style={{width: width * 0.35, aspectRatio: 1}}
-        resizeMode="contain"
-      />
-    </View>
-  );
 };
 
 export default ChallengesBanner;
