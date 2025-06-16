f99cf5659845895bf4e61f5cbc774e977b273e67
jeyaprakashp4587 <jeyaprakashp022@gmail.com>
2025-06-16 08:25:36 +0530

    apic ahges

--- Api.js
+++ Api.js
@@ -1,4 +1,4 @@
-import {APIKEY} from '@babel/preset-env';
+import {APIKEY} from '@env';
 const Api = !__DEV__ ? `${APIKEY}` : 'http://192.168.43.90:8080';
 const SocketApi = !__DEV__ ? `${APIKEY}` : 'http://192.168.43.90:8080';
 const loginApi = !__DEV__ ? `${APIKEY}` : 'http://192.168.43.90:8080';
