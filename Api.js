bce9427fa8598dba89914d3a87ae33f8a4552788
jeyaprakashp4587 <jeyaprakashp022@gmail.com>
2025-06-16 08:24:07 +0530

    git add env

--- Api.js
+++ Api.js
@@ -1,21 +1,10 @@
-const Api = !__DEV__
-  ? 'http://ec2-50-16-176-155.compute-1.amazonaws.com:8080'
-  : 'http://192.168.43.90:8080';
-const SocketApi = !__DEV__
-  ? 'http://ec2-50-16-176-155.compute-1.amazonaws.com:8080'
-  : 'http://192.168.43.90:8080';
-const loginApi = !__DEV__
-  ? 'http://ec2-50-16-176-155.compute-1.amazonaws.com:8080'
-  : 'http://192.168.43.90:8080';
-const profileApi = !__DEV__
-  ? 'http://ec2-50-16-176-155.compute-1.amazonaws.com:8080'
-  : 'http://192.168.43.90:8080';
-const functionApi = !__DEV__
-  ? 'http://ec2-50-16-176-155.compute-1.amazonaws.com:8080'
-  : 'http://192.168.43.90:8080';
-const challengesApi = !__DEV__
-  ? 'http://ec2-50-16-176-155.compute-1.amazonaws.com:8080'
-  : 'http://192.168.43.90:8080';
+import {APIKEY} from '@babel/preset-env';
+const Api = !__DEV__ ? `${APIKEY}` : 'http://192.168.43.90:8080';
+const SocketApi = !__DEV__ ? `${APIKEY}` : 'http://192.168.43.90:8080';
+const loginApi = !__DEV__ ? `${APIKEY}` : 'http://192.168.43.90:8080';
+const profileApi = !__DEV__ ? `${APIKEY}` : 'http://192.168.43.90:8080';
+const functionApi = !__DEV__ ? `${APIKEY}` : 'http://192.168.43.90:8080';
+const challengesApi = !__DEV__ ? `${APIKEY}` : 'http://192.168.43.90:8080';
 // -----
 module.exports = {
   Api,
